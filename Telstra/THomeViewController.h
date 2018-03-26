38d8f7c36280ee45a983a51ada28467e69b81a0c
manzar_hussain <manzar_hussain@infosys.com>
2018-03-26 15:52:18 +0530

    Test cases and schema issue resolved

--- Telstra/THomeViewController.h
+++ Telstra/THomeViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface THomeViewController : UIViewController
+@interface THomeViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @end
